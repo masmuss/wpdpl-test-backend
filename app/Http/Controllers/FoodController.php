3733586fc06ba2ec8e8e2ac59ac3d9268eabc957
masmuss <ahmusafir.khoirul@gmail.com>
2023-02-28 11:21:15 +0700

    initial project
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

---
+++ app/Http/Controllers/FoodController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Food $food)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Food $food)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Food $food)
+    {
+        //
+    }
+}
